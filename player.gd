54eeee7a6e87d82ba89bf3699746b17c3778e300
lochiethornee-dev <lochiethornee@gmail.com>
2026-03-13 23:08:57 +1030

    Added lots of enemy, damage stuff, and intro screen text images etc

--- player.gd
+++ player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var bulletScene = load("res://playerbullet.tscn")
 
+
+func _ready() -> void:
+	var bullet = bulletScene.instantiate()
+	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -9,3 +14,8 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	if Input.is_action_just_pressed("shoot") and %cooldown.is_stopped():
+		var bullet = bulletScene.instantiate()
+		add_sibling(bullet)
+		bullet.global_position = self.global_position
+		%cooldown.start()
---
+++ playerbullet.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var speed = 600
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += speed * delta
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage()
+		queue_free()
