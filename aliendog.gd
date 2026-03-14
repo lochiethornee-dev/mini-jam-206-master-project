02fb02bda8786898c62e8769538a99f03846c629
lochiethornee-dev <lochiethornee@gmail.com>
2026-03-14 23:42:10 +1030

    alien dog and extra art implemented

--- aliendog.gd
+++ aliendog.gd
@@ -1,12 +1,16 @@
 extends StaticBody2D
 
-var health = 1
+var health = 0
 var speed = 350
-
-
+var yspeed = 100
+var switch = false
 
 func _physics_process(delta: float) -> void:
 	position.x += -speed * delta
+	if switch == false:
+		position.y += -yspeed * delta
+	elif switch == true:
+		position.y += yspeed * delta
 
 
 
@@ -31,3 +35,10 @@ func take_damage():
 		%Sprite2D.modulate = Color(0.788, 0.796, 0.639, 1.0)
 		await get_tree().create_timer(.1).timeout
 		%Sprite2D.modulate = Color(1,1,1)
+
+
+func _on_switch_timeout() -> void:
+	if switch == true:
+		switch = false
+	elif switch == false:
+		switch = true
--- enemy_manager.gd
+++ enemy_manager.gd
@@ -1,20 +1,22 @@
 extends Node2D
 
 var shooter1spawned = 0
+var dogspawned = 0
 var intro = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%alienshooter1timer.wait_time = 15
 	%alienshooter1timer.start()
-	
+	%aliendoggytimer.wait_time = 45
+	%aliendoggytimer.start()
 	
 	
 
 
 
 ####################
-#ALIEN SHOOTER 1   #
+#  ALIEN SHOOTER 1 #
 ####################
 func spawn_alienshooter1():
 	var newshooter1 = preload("res://alienshooter.tscn").instantiate()
@@ -28,8 +30,31 @@ func spawn_alienshooter1():
 	#%alienshooter1timer.wait_time = %alienshooter1timer.wait_time - .5
 	shooter1spawned += 1
 	if shooter1spawned == 3:
-		%alienshooter1timer.wait_time = 200
+		%alienshooter1timer.wait_time = 20
 		%alienshooter1timer.start()
 
 func _on_alienshooter_1_timer_timeout() -> void:
 	spawn_alienshooter1()
+
+
+
+####################
+#    ALIEN DOGGY   #
+####################
+
+func _on_aliendoggytimer_timeout() -> void:
+	spawn_aliendoggy()
+
+func spawn_aliendoggy():
+	var newdog = preload("res://aliendog.tscn").instantiate()
+	add_child(newdog)
+
+	%dogpath.progress_ratio = randf()
+	newdog.global_position = %dogpath.global_position
+	if %aliendoggytimer.wait_time > 2:
+		%aliendoggytimer.wait_time = 2
+	%aliendoggytimer.start()
+	dogspawned += 1
+	#if dogspawned == 3:
+		#%aliendoggytimer.wait_time = 200
+		#%aliendoggytimer.start()
